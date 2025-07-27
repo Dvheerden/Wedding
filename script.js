92a32f22dff4f959afbe9f07908df444aaec2b46
Dvheerden <dvheerden@hotmail.co.za>
2025-07-27 22:00:18 +0800

    Change to gtranslate

--- script.js
+++ script.js
@@ -288,12 +288,4 @@ document.addEventListener('DOMContentLoaded', () => {
     console.error('Notification elements not found!');
   }
 });
-function googleTranslateElementInit() {
-  new google.translate.TranslateElement({
-    pageLanguage: 'en',
-    includedLanguages: 'af,en,fr,de,hi,pt,sn,tl,tn,xh,zu,ja,zh-CN,zh-TW',
-    layout: google.translate.TranslateElement.InlineLayout.SIMPLE,
-    autoDisplay: false,
-    defaultLanguage: 'en',
-  }, 'google_translate_element');
-}
+
